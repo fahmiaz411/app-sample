03c29da251374beafe7927a140a89618388256f7
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-03 17:39:01 +0700

    sample get

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fahmiaz411/app-sample
+
+go 1.17
+
+require github.com/fahmiaz411/go-sample v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_sample "github.com/fahmiaz411/go-sample"
+)
+
+func main() {
+	fmt.Println(go_sample.Hello())
+}
